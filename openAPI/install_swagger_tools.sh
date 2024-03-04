34ae8e3242744d8a8e65b04cebdf9b5fb0e83a4b
Benedikt <mail@benedikt-eickhoff.de>
2024-03-04 13:08:58 +0000

    update install_swagger_tools.sh after drprecation warnings

--- openAPI/install_swagger_tools.sh
+++ openAPI/install_swagger_tools.sh
@@ -10,9 +10,9 @@ sudo apt-get update
 echo "Installing jq..."
 sudo apt-get install -y jq
 
-echo "Installing Node.js and npm..."
-# Using NodeSource's binary distributions for a more recent version of Node.js
-curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+echo "Installing Node.js 18 LTS (Hydrogen) and npm..."
+# Using NodeSource's binary distributions for Node.js 18 LTS
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 # Verify installation
